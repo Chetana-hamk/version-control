a79933e2652e9271adeac29e23be9e34238a8f8e
Your Name <amk1002489@student.hamk.fi>
2024-10-25 11:33:07 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+       Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();;
+
+        if(name.equals(""))
+        {
+            System.out.println("Error");
+   
+        } 
+        else
+{
+         System.err.println("our name is " + name + ".");
+}}
+    }
